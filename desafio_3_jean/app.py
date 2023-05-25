cc8ecd0d0f3b080a02b3e4e2b5417f55c669f4fd
Vitor Saborito <110786874+VituuSaborito@users.noreply.github.com>
2023-05-25 20:57:57 -0300

    desafio 3

---
+++ desafio_3_jean/app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, url_for, request
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+
+# conexão com o banco de dados
+app.config['MYSQL_Host'] =  '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'contatos'
+
+mysql = MySQL(app)
+
+
+@app.route("/")
+def main():
+    return render_template('home.html')
+
+@app.route("/home/")
+def home():
+    return render_template('home.html')
+
+@app.route("/quemsomos/")
+def quemsomos():
+    return render_template('quemsomos.html')
+
+# @app.route("/contato/")
+# def contato():
+#     return render_template('contato.html')
+
+
+@app.route('/contato/', methods=['GET', 'POST'])
+def contato():
+    if request.method == "POST":
+        email = request.form['email']
+        assunto = request.form['assunto']
+        descricao = request.form['descricao']
+        
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO contatos(email, assunto, descricao) VALUES (%s, %s, %s)", (email, assunto, descricao))
+       
+        mysql.connection.commit()
+        
+        cur.close()
+
+        return 'sucesso'
+    return render_template('contato.html')
+
+
+
+# rota usuários para listar todos os usuário no banco de dados.
+@app.route('/users/')
+def users():
+    cur = mysql.connection.cursor()
+
+    users = cur.execute("SELECT * FROM contatos")
+
+    if users > 0:
+        userDetails = cur.fetchall()
+
+        return render_template("users.html", userDetails=userDetails)
+
+    return render_template('users.html')
---
+++ desafio_3_jean/contatos.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE contatos;
+USE contatos;
+
+CREATE TABLE contatos(
+email VARCHAR (70),
+assunto VARCHAR (70),
+descricao VARCHAR (300)
+);
+
+
+
